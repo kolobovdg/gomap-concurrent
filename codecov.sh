ebb1f9a9948c298119d6927d17a5f8a6acab0790
kolobov <kolobovdg@yandex.ru>
2019-07-23 01:29:55 +0300

    codecov please work

--- codecov.sh
+++ codecov.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > ./coverage.txt
 
-go test -race -coverprofile=profile.out -covermode=atomic "$d"
+go test -v -race -coverprofile=profile.out -covermode=atomic ./...
 if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
     rm profile.out
